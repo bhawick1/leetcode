9f9dce3fe8845813ccfd02cd8fc68a1cc9026b67
bhawick1 <kaushalbhawick@gmail.com>
2026-07-31 00:55:52 +0530

    Time: 5 ms (80.83%), Space: 45.4 MB (99.05%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+        return false;
+        int copy = x;
+        int rev=0;
+       while(x!=0)
+       {
+        int d=x%10;
+         rev=rev*10+d;
+        x=x/10;
+       } 
+       if (copy==rev)
+       return true;
+       else 
+      return false;
+
+}}
